dab2ac482982e711c7f23a4d874e0112f9d376c8
Sail-il <sail.il.tau@gmail.com>
2022-03-31 16:46:18 +0300

    telemtry update

--- catkin_ws/src/communication/src/scripts/telemtry.py
+++ catkin_ws/src/communication/src/scripts/telemtry.py
@@ -5,7 +5,7 @@ import rosbag
 from datetime import datetime
 #import std_msgs
 #from visualization_msgs import MarkerArray
-from sensor_msgs.msg import LaserScan, PointCloud2, Imu
+from sensor_msgs.msg import LaserScan, PointCloud2, Imu, Image
 
 BUFFER_MAX = 10
 
@@ -19,16 +19,17 @@ IMU_TOPIC = "/imu/data"
 class Laser:
     def __init__(self):
         self.bag    = rosbag.Bag('Laser' + datetime.now().strftime('%d_%m_%Y__%H_%M'), 'w')
+        self.subscriber = rospy.Subscriber(CV_IMAGE_RECT_TOPIC, PointCloud2, self.PointCloudCallback)
 
-    def LaserScan_data_save(self, data):
-        self.bag.write(POINTCLOUD_TOPIC, data)
+    def PointCloudCallBack(self, msg):
+        self.bag.write(POINTCLOUD_TOPIC, msg)
+        rospy.loginfo("recived msg from POINTCLOUD_TOPIC!\n")
 
 class cv:
     def __init__(self):
-        self.bag_lock = 0
         self.bag = rosbag.Bag('cv' + datetime.now().strftime('%d_%m_%Y__%H_%M'), 'w')
-        self.raw_subscribe = rospy.Subscriber(CV_IMAGE_RAW_TOPIC, Image_Raw, self.ImageRawCallback)
-        self.rect_subsvribe = rospy.Subscriber(CV_IMAGE_RECT_TOPIC, Image_Rect, self.ImageRectCallback)
+        self.raw_subscribe = rospy.Subscriber(CV_IMAGE_RAW_TOPIC, Image, self.ImageRawCallback)
+        self.rect_subsvribe = rospy.Subscriber(CV_IMAGE_RECT_TOPIC, Image, self.ImageRectCallback)
         self.pointcloud_subscribe = rospy.Subscriber(CV_IMAGE_RECT_TOPIC, PointCloud2, self.PointCloudCallback)
     
     def ImageRawCallBack(self, msg):
@@ -42,6 +43,7 @@ class cv:
     def PointCloudCallBack(self, msg):
         self.bag.write(CV_POINTCLOUD_TOPIC, msg)
         rospy.loginfo("CV pointcloud msg recived")
+
 class imu:
     def __init__(self):
         self.bag = rosbag.Bag('Imu' + datetime.now().strftime('%d_%m_%Y__%H_%M'), 'w')
@@ -54,29 +56,23 @@ class imu:
 
 class subscribe_control:
     def __init__(self):
-        self.data = data_buff
-        self.cv_subscriber = cv()
+        self.cv_data = cv()
         self.Imu_data = imu()
-        self.Laser = Laser()
-    #def cv_msg_callback(self, msg):
-        #rospy.loginfo(msg.markers)
-        #self.data.cv_data_set(msg.Markers)
+        self.Laser_data = Laser()
     
-    def PointCloudCallBack(self, msg):
-        self.data.Laser_data.LaserScan_data_save(msg)
-        #rospy.loginfo("recived msg from POINTCLOUD_TOPIC!\n")
 
 if __name__ == '__main__':
     #new_bag = rospy.get_param("/start_new_bag")
 
     try:
         rospy.init_node('communication')
-        sub_ctrl = subscribe_control()
+        main = subscribe_control()
         #if new_bag == True:
         #    data.Laser_data.bag.close()
         #    data.Laser_data.bag    = rosbag.Bag('Laser' + datetime.now().strftime('%d_%m_%Y__%H_%M'), 'w')
         #    new_bag = False
         rospy.spin()
     finally:   
-        data.Laser_data.bag.close()
-        data.Imu_data.bag.close()
+        main.Laser_data.bag.close()
+        main.Imu_data.bag.close()
+        main.cv_data.bag.clode()

34642a11eca3f88c4c904747fede4ee15ab24228
Sail-il <sail.il.tau@gmail.com>
2022-05-02 16:58:03 +0300

    delete catkin_ws_1

--- catkin_ws_1/build/pc2l/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/blacksail/catkin_ws/src/pc2l
-# Build directory: /home/blacksail/catkin_ws/build/pc2l
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
